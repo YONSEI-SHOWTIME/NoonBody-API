46e2ad85af163e4460ea21c10b7ab8204a875a41
전준형 <jjuni@jeonjunhyeong-ui-MacBookPro.local>
2023-01-11 23:35:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/showtime/noonbody/NoonBodyApplication.java
@@ -0,0 +1,13 @@
+package com.showtime.noonbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonBodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoonBodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/showtime/noonbody/NoonBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showtime.noonbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoonBodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
